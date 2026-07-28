dedf038611d45c604757423443e5aa3b6fadbadd
【﻿ＫＲＥＳＳＷＥＬＬ】 <help@infinityhackers.tech>
2026-07-28 21:31:59 +0300

    Add html package import to main.go

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/net/html"
 )
 
 type TrackerItem struct {
