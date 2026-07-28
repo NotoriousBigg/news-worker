02057366dd2607cbc12eaf6c0324cd5f7076f495
【﻿ＫＲＥＳＳＷＥＬＬ】 <help@infinityhackers.tech>
2026-07-28 20:43:29 +0300

    Fix key name from 'size' to 'level' in heading block

--- main.go
+++ main.go
@@ -166,8 +166,7 @@ func processNews(collection *mongo.Collection, token, chatID string, linkRegex *
 			blocks = append(blocks, map[string]interface{}{
 				"type":  "heading",
 				"size": 1, // Fix: Changed key name from 'size' to 'level'
-				"text": article.Headline, 
-				},
+				"text": article.Headline,
 			})
 
 			// 2. Swappable Slideshow Grid Layout block
